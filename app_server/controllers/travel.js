ad8b7af7189270d2b363d2274c86217358d760c0
Ken Rodriguez <ken.rodriguez@protonmail.com>
2023-05-28 16:22:23 -0500

    convert travel template from static data to JSON for scalability

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,10 @@
+var fs = require('fs');
+
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+// get travel view
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways - Travel' });
+    res.render('travel', { title: 'Travlr Getaways - Travel', trips });
 };
 
 module.exports = {
