190ec6a125d0dc139e4a9cd95534693c9ca7339e
Ken Rodriguez <ken.rodriguez@protonmail.com>
2023-06-11 19:18:24 -0500

    polished and completed API

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,12 +1,54 @@
-var fs = require('fs');
-
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
-
-// get travel view
-const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways - Travel', trips });
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
 };
 
+// get travel view
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,     //backticks for days
+        method: 'GET',
+        json: {},
+    };
+
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    )
+}
+
+// internal method to render the travel list
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = 'Travlr Getaways';
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    }
+    else {
+        if (!responseBody.length) {
+            message = 'No trips exist in our database!';
+        }
+    }
+    res.render('travel',
+        {
+            title: pageTitle,
+            trips: responseBody,
+            message
+        }
+    );
+}
+
+
 module.exports = {
-    travel
+    travelList,
+    renderTravelList
 };
--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -3,6 +3,6 @@ var router = express.Router();
 const controller = require('../controllers/travel');
 
 /* GET homepage */
-router.get('/', controller.travel);
+router.get('/', controller.travelList);
 
 module.exports = router;
